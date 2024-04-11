5baa54a9ead54d0c5ebd08109488b92dc46b56b5
eanassu <eanassu@gmail.com>
2024-04-10 21:02:03 -0300

    aula 10/04

--- app/src/main/java/br/fmu/aula4an/DatabaseActivity.java
+++ app/src/main/java/br/fmu/aula4an/DatabaseActivity.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class DatabaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
-
-        Funcionario f = new Funcionario();
-
-        System.out.println(f);
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        Funcionario f = new Funcionario(1,"teste1",new Date(),1000);
+        dao.insert(f);
     }
 }
